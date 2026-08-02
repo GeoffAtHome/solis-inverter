9674267120e4224aac0336212304e06d7fba6db9
Geoff <Geoff.Soord@cardiffandvalescouts.org.uk>
2026-08-02 14:10:23 +0100

    Remove debug logging for specific response messages in send_request method

--- solis/solis.py
+++ solis/solis.py
@@ -75,15 +75,6 @@ class Inverter:
         try:
             response = await self.solisClient.request(msg, msg_id)
             if response != None:
-                if msg_id == 102:
-                    log.debug(
-                        "Received response msg_id=%s start=%s end=%s len=%s raw=%s",
-                        msg_id,
-                        start,
-                        end,
-                        len(response),
-                        response[: len(response)],
-                    )
                 params.parse(response, start - 1, length, msg_id)
         except Exception as e:
             log.warning(
